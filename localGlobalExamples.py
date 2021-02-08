383144de37e384b335db3c313a10435cecfb2250
swapnil <swapnil.purandar@gmail.com>
2021-02-08 07:37:55 +0530

    Add local global variable example

--- localGlobalExamples.py
+++ localGlobalExamples.py
@@ -2,8 +2,23 @@ x = 10
 
 def foo():
     global x
-    x = 5
+    x = x + 5
     print('X inside: ', x)
 
 foo()
 print('x outside: ', x)
+
+def foofa():
+    x = 20
+
+    def bar():
+        global x
+        x = 25
+
+    print('Value of x before calling bar: ', x)
+    print('Cslling bar now')
+    bar()
+    print('Value of x after calling bar: ', x)
+
+foofa()  
+print('Value of x in main: ', x)
